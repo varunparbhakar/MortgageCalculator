8cd8542210a786b58180d2f33ce50911acc4f145
vaunparbhakar <47151470+vaunparbhakar@users.noreply.github.com>
2021-09-13 13:18:37 -0700

    Made some new varable for tracking the amound of time that is required in order to cover up the refinance cost

--- CalculatorMain.py
+++ CalculatorMain.py
@@ -1,12 +1,13 @@
 # Helper Variables
 paidBalance = 0
 interestPaid = 0
+totalPaid = 0
 
 lumpSumPayOff = 0  # One time payment made to reduce only principal
-principalAmount = 474000 - lumpSumPayOff
+principalAmount = 470000 + 0 + lumpSumPayOff
 payOffYear = 30
-interestRateAPR = 0.375
-currentProperty_Value = 680000
+interestRateAPR = 0.0265
+currentProperty_Value = 700000
 
 propertyAnnualGrowthRate = 0.0103  # Annual property value growth rate
 monthlyInterestRate = interestRateAPR / 12
@@ -14,9 +15,16 @@ payments = payOffYear * 12
 additionalMonthlyPayment = 0
 monthlyVariable = interestRateAPR / payments  # APR interest rate / 360
 
+currentMonthlyMortgage = 3200
+refinaceCost = 25000
+monthlySaving = 0
+coverUpMonths = 0
+
 monthlyPayment = ((principalAmount * monthlyInterestRate) * (1 + monthlyInterestRate) ** payments) / (
         ((1 + monthlyInterestRate) ** payments) - 1) + additionalMonthlyPayment
-annualEscrow = (currentProperty_Value * 0.0098) + 2050
+print("The projected monthly payment: ", monthlyPayment)
+propertyTaxRate = 0.0119
+annualEscrow = (currentProperty_Value * propertyTaxRate) + 2050
 
 year = 0
 moneyLeft = True
@@ -39,21 +47,34 @@ while moneyLeft:
             monthlyInterest = principalAmount * (interestRateAPR / 12)
             monthlyPrinciplePay = monthlyPayment - monthlyInterest
             principalAmount -= monthlyPrinciplePay
-
             totalMonthPay = (monthlyPrinciplePay + monthlyInterest + escrowMonthly)
+
+
             print("{:2} {:15.2f}{:18.2f}{:18.2f}{:18.2f}{:18.2f}".format(month + 1, totalMonthPay, monthlyPrinciplePay,
                                                                          monthlyInterest, escrowMonthly,
                                                                          principalAmount))
             paidBalance += monthlyInterest + monthlyPrinciplePay
+            totalPaid += totalMonthPay
             interestPaid += monthlyInterest
 
+            if (monthlySaving == 0):
+                monthlySaving = currentMonthlyMortgage - totalMonthPay
+                coverUpMonths = refinaceCost/monthlySaving
+
+    print()
     year = year + 1
     currentProperty_Value += currentProperty_Value * propertyAnnualGrowthRate
-    annualEscrow = (currentProperty_Value * 0.0098) + 2050
+    annualEscrow = (currentProperty_Value * propertyTaxRate) + 2050
+    print("Total payment Paid", totalPaid)
 
 monthlyPayment2 = (principalAmount * monthlyInterestRate) / (1 - ((1 + monthlyInterestRate) ** (-1 * payments)))
+coverupRemainingMonths = int(coverUpMonths%12)
+coverUpYears = (coverUpMonths/12)
 
 print("You will play off the full loan in ", year, "years and", month, "months")
+print("Total Interets Paid",interestPaid)
+print("Total payment Paid",totalPaid)
+print("You will recover the refinance cost in",int(coverUpMonths/12), "years and in", coverupRemainingMonths, "months")
 
 # print(monthlyPayment+700)
 # print("Total Monthly Payment: {:.2f}".format(monthlyPayment))
